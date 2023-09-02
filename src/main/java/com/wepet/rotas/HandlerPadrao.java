cb2fb68ce38785048c4b0031f2b5322751cb2927
ricardo.demeterko <ninodemeterko@hotmail.com>
2023-09-02 15:20:49 -0300

    criado bando de dados

--- src/main/java/com/wepet/rotas/HandlerPadrao.java
+++ src/main/java/com/wepet/rotas/HandlerPadrao.java
@@ -11,7 +11,9 @@ public class HandlerPadrao implements HttpHandler {
     @Override
     public void handle(HttpExchange pedido) throws IOException {
         System.out.println((this.getClass().getSimpleName()) +": Requisição recebida - " + pedido.getRequestURI().toString());
+
         String response = "Erro 404";
+        
         pedido.sendResponseHeaders(404, response.length());
         OutputStream os = pedido.getResponseBody();
         os.write(response.toString().getBytes());
--- src/main/java/com/wepet/rotas/versaoHandler.java
+++ src/main/java/com/wepet/rotas/versaoHandler.java
@@ -10,8 +10,10 @@ public class versaoHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange pedido) throws IOException {
-        System.out.println("Requisição recebida - " + pedido.getRequestURI().toString());
+        System.out.println((this.getClass().getSimpleName()) +": Requisição recebida - " + pedido.getRequestURI().toString());
+        
         String response = "0.0.1";
+
         pedido.sendResponseHeaders(200, response.length());
         OutputStream os = pedido.getResponseBody();
         os.write(response.toString().getBytes());
