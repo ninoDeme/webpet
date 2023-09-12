ca476c8ea8aab10a52fdecdd351466143f77e69f
Eder Melo <edermelo8906@gmail.com>
2023-09-11 21:01:31 -0300

    tabela produto

--- banco.sql
+++ banco.sql
@@ -1,50 +1,65 @@
-
 /* 
-    Arquivo com as instrucoes para recriar o banco de dados
-    Sempre ao rodar qualquer comando que cria ou modifica uma tabela salve ele aqui
-
-    exemplo:
-
-    -- criar tabela Usuario
-
-    CREATE TABLE usuario (
-        id_usuario INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, -- id do usuario, numero nao nulo chave primaria que vai ser usado como identificador unico
-        nome TEXT NOT NULL UNIQUE, -- nome do usuario, string nao nula unica
-        senha TEXT NOT NULL, -- senha do usuario, string nao nula
-        email TEXT NOT NULL UNIQUE, -- email do usuario, string nao nula unica
-        foto TEXT, -- foto de perfil do usuario, caminho da foto no servidor ex: C:/imagens/a23SDq2d2.png
-    );
-
-    CREATE TABLE favoritos_usuario (
-        id_usuario   INTEGER PRIMARY KEY,
-        id_produto TEXT    NOT NULL,
-        tipo      INTEGER NOT NULL,
-        FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario) 
-        FOREIGN KEY (id_produto) REFERENCES produto (id_produto) 
-    );
-*/
-
+ Arquivo com as instrucoes para recriar o banco de dados
+ Sempre ao rodar qualquer comando que cria ou modifica uma tabela salve ele aqui
+ 
+ exemplo:
+ 
+ -- criar tabela Usuario
+ 
+ CREATE TABLE usuario (
+ id_usuario INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, -- id do usuario, numero nao nulo chave primaria que vai ser usado como identificador unico
+ nome TEXT NOT NULL UNIQUE, -- nome do usuario, string nao nula unica
+ senha TEXT NOT NULL, -- senha do usuario, string nao nula
+ email TEXT NOT NULL UNIQUE, -- email do usuario, string nao nula unica
+ foto TEXT, -- foto de perfil do usuario, caminho da foto no servidor ex: C:/imagens/a23SDq2d2.png
+ );
+ 
+ CREATE TABLE favoritos_usuario (
+ id_usuario   INTEGER PRIMARY KEY,
+ id_produto TEXT    NOT NULL,
+ tipo      INTEGER NOT NULL,
+ FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario) 
+ FOREIGN KEY (id_produto) REFERENCES produto (id_produto) 
+ );
+ */
 create table usuario(
     id_usuario INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
-    senha NOT NULL, 
+    senha NOT NULL,
     email TEXT NOT NULL UNIQUE,
     telefone TEXT NOT NULL
-    );
+);
+
 CREATE TABLE ANIMAL(
     id_animal INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL, 
-    descricao TEXT, 
+    nome TEXT NOT NULL,
+    descricao TEXT,
     imagem TEXT
-    );
-        CREATE TABLE categoria(
-        id_categoria INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-        nome TEXT NOT NULL,
-        descricao TEXT
-    );
+);
+
+CREATE TABLE categoria(
+    id_categoria INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    nome TEXT NOT NULL,
+    descricao TEXT
+);
+
 CREATE TABLE fabricante(
     id_fabricante INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     nome TEXT NOT NULL,
     descricao TEXT,
     logo TEXT NOT NULL
-    );
+);
+
+CREATE table produto(
+    id_animal INTEGER,
+    id_categoria INTEGER,
+    id_fabricante INTEGER,
+    id_produto INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    descricao TEXT,
+    preco DOUBLE,
+    quantidade INTEGER NOT NULL,
+    FOREIGN KEY(id_animal) REFERENCES ANIMAL(id_animal),
+    FOREIGN KEY(id_categoria) REFERENCES categoria(id_categoria),
+    FOREIGN KEY(id_fabricante) REFERENCES fabricante(id_fabricante)
+);
